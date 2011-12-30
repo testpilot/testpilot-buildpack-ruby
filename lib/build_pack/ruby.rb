b10e9e8fe784c29088e79e24bc9afb15890ee655
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-12-30 17:14:22 +1100

    Added constants from Heroku
    We should make these our own soon to match our environment

--- lib/build_pack/ruby.rb
+++ lib/build_pack/ruby.rb
@@ -4,6 +4,12 @@ require "build_pack/base"
 
 # base Ruby Language Pack. This is for any base ruby app.
 class BuildPack::Ruby < BuildPack::Base
+  LIBYAML_VERSION     = "0.1.4"
+  LIBYAML_PATH        = "libyaml-#{LIBYAML_VERSION}"
+  BUNDLER_VERSION     = "1.1.rc.6"
+  BUNDLER_GEM_PATH    = "bundler-#{BUNDLER_VERSION}"
+  NODE_VERSION        = "0.4.7"
+  NODE_JS_BINARY_PATH = "node-#{NODE_VERSION}"
 
   # detects if this is a valid Ruby app
   # @return [Boolean] true if it's a Ruby app
